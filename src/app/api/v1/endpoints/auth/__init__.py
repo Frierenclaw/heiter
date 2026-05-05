e40eee01e23ce2fc04db571fdf4b46ce8e91da1e
ladev <aleksei.lapochkin1@gmail.com>
2026-05-05 21:28:37 +0300

    Улучшена обработка ошибок в gpt эндпоинте

--- src/app/api/v1/endpoints/auth/__init__.py
+++ src/app/api/v1/endpoints/auth/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints.auth import login
+from api.v1.endpoints.auth import login, register
 
 base_auth_router = APIRouter(prefix='/auth')
 
 base_auth_router.include_router(router=login.router,
                                 tags=['Login'])
+base_auth_router.include_router(router=register.router,
+                                tags=['Register'])
--- src/app/api/v1/endpoints/gpt/create_completions.py
+++ src/app/api/v1/endpoints/gpt/create_completions.py
@@ -1,6 +1,8 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from loguru import logger
+from openai import APITimeoutError, OpenAIError, RateLimitError
 from openai.types.chat.chat_completion import ChatCompletion
 
 from api.v1.dependencies.auth import get_current_user
@@ -16,11 +18,32 @@ async def create_chat_completions(dto: CreateCompletionRequestDTO,
                                   user: Annotated[User, Depends(get_current_user)]):
     messages = [{'role': message.role, 'content': message.content} for message in dto.messages]
 
-    request = await async_openai_client.chat.completions.create(
-        model=dto.model,
-        messages=messages,
-        max_tokens=dto.max_tokens,
-        temperature=dto.temperature
-    )
+    try:
+        request = await async_openai_client.chat.completions.create(
+            model=dto.model,
+            messages=messages,
+            max_tokens=dto.max_tokens,
+            temperature=dto.temperature
+        )
+    except RateLimitError as e:
+        logger.error(f'GPT Rate limit has exceeded . Detail error: {e}. Request by user {user.id}')
+        
+        raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS,  # noqa: B904
+                            detail='Global rate limit has exceeded') 
+    except APITimeoutError as e:
+        logger.error(f'GPT Timeout. Detail error: {e}')
 
-    return request # codestral-latest
+        raise HTTPException(status_code=status.HTTP_504_GATEWAY_TIMEOUT,  # noqa: B904
+                            detail='Internal request timeout. Try again later')
+    except OpenAIError as e:
+        logger.error(f'OpenAI Error. Detail error: {e}')
+
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,  # noqa: B904
+                            detail='Internal error. Try again later')
+    except Exception as e:
+        logger.error(f'Unexcepted error. Detail error: {e}')
+        
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,  # noqa: B904
+                            detail='Internal server error. Try again later')
+    
+    return request
